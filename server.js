78c30a8b760018be725e3103c4163fe097a931c0
darthvader1118 <kyellayi@gmail.com>
2016-11-21 18:33:33 -0500

    forget js libraries

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var request = require('request'); // "Request" library
+var querystring = require('querystring');
+var cookieParser = require('cookie-parser');
 var app = express();
 //Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(__dirname + '/public'));
